06d258992231fd38877b2a2278cfcc504da28ea1
jluna-meli <jessica.deluna@mercadolibre.com.mx>
2022-11-17 07:19:10 -0600

    [UPATE_ROUTES]Queue endpoints

--- routes/queue.py
+++ routes/queue.py
@@ -1,104 +1,84 @@
-import json
-from time import strftime
-import time
+
 import redis
+import werkzeug
 from rq import Queue
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, current_app
+from werkzeug.exceptions import Unauthorized
+
+from config.function_jwt import validate_token
 
 routes_queue = Blueprint("queue", __name__)
+@routes_queue.before_request
+def verify_token_middleware():
+    try:
+        token = request.headers['Authorization'].split(" ")[1]
+        return validate_token(token, output=False)
+    except:
+        return jsonify({"status": "Internal Error"}), 501
 
 
 r = redis.Redis(host="redis-container",port=6379, db=0)
 q = Queue(connection=r)
 
-@routes_queue.route("/push", methods=["POST"] )
-def push_m_redis():
+@routes_queue.route("/connection/ping", methods=["GET"] )
+def connection_Redis():
+    try:
+        ping = "Connection successful" if r.ping() else "Ups, something happends with Redis Connection"
 
-    data = request.get_json()
-    message = data["msg"]
-    job = r.mset({"msg": message})
-
-    print(r.get("msg"))
-    result = {
-        "msg": job,
-        "Time": strftime('%a, %d %b %Y %H:%M:%S')
-    }
-    return json.dumps({"result": result})
-
-
-def create_task(task_type):
-    time.sleep(int(task_type) * 10)
-    return True
-
-@routes_queue.route("/push/enqueue", methods=["POST"] )
-def create_message_with_enqueue():
-    task_type = request.get_json()
-
-    task = q.enqueue(create_task, task_type)
-    print(q.get_job_ids())
-    print(q.get_jobs())
-    print(q.count)
-    response_object = {
-        "status": "success",
-        "data": {
-            "task_id": task.get_id()
+        response_object = {
+            "status": ping,
         }
-    }
-    return jsonify(response_object), 201
+        return jsonify(response_object), 200 if r.ping() else 400
+    except:
+        return jsonify({"status": "Internal Error"}), 501
 
-@routes_queue.route("/count/enqueue", methods=["GET"] )
-def count_messages_with_enqueue():
 
-    print(q.get_job_ids())
-    print(q.get_jobs())
-    response_object = {
-        "status": "0k",
-        "count": q.count
-    }
-    return jsonify(response_object), 200
-
-@routes_queue.route("/count2", methods=["GET"] )
+@routes_queue.route("/count", methods=["GET"] )
 def count_messages_with_Redis():
-    total = r.llen(('Messages'))
-    response_object = {
-        "status": "0k",
-        "count": total
-    }
-    return jsonify(response_object), 200
+    try:
+        total = r.llen(('Messages'))
+        response_object = {
+            "status": "0k",
+            "count": total
+        }
+        return jsonify(response_object), 200
+    except:
+        return jsonify({"status": "Internal Error"}), 501
 
-@routes_queue.route("/push2", methods=["POST"] )
+@routes_queue.route("/push", methods=["POST"] )
 def create_r_redis():
+    try:
+        if request.get_json()['msg'] == None:
+            return jsonify({"error": "Message canot be a null"}), 400
 
-    print(r.keys())
-    r.lpush('Messages', request.get_json()['msg'])
+        if isinstance(request.get_json()['msg'], str) == False:
+            return jsonify({"error": "The message should be a string "}), 400
+        r.lpush('Messages', request.get_json()['msg'])
 
-    print(r.llen(('Messages')))
-    l = r.lrange('Messages', 0,r.llen(('Messages'))-1)
-    for x in l:
-        print(x)
+        response_object = {
+            "status": "success"
+        }
+        return jsonify(response_object), 201
+    except:
+        return jsonify({"status": "Internal Error"}), 501
 
-    # Elimina la lista
-    #while (r.llen('LanguageList')!= 0):
-    #    print(r.lpop('LanguageList'))
-    response_object = {
-        "status": "success"
-    }
-    return jsonify(response_object), 200
+
+
 
 @routes_queue.route("/pop", methods=["DELETE"] )
 def delete_r_redis():
+    try:
+        if r.llen(('Messages')) == 0:
+            return jsonify({"error": "Its not possible to delete any message because there not messages in queue"}), 400
 
-    print(r.keys())
-    msg = r.lindex('Messages',0).decode()
-    r.lpop('Messages')
+        msg = r.lindex('Messages',0).decode()
+        r.lpop('Messages')
 
-    print(r.llen(('Messages')))
-    l = r.lrange('Messages', 0,r.llen(('Messages'))-1)
-    for x in l:
-        print(x)
+        response_object = {
+            "status": "success",
+            "msg": msg
+        }
+        return jsonify(response_object), 200
+    except:
+        return jsonify({"status": "Internal Error"}), 501
 
-    response_object = {
-        "status": "success",
-        "msg": msg
-    }
-    return jsonify(response_object), 200
