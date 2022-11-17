458fa60e3d5953a5e94e1101a6ceb800ca1bc178
jluna-meli <jessica.deluna@mercadolibre.com.mx>
2022-11-17 07:18:43 -0600

    [UPATE_ROUTES] Auth configuration

--- routes/auth.py
+++ routes/auth.py
@@ -1,19 +1,27 @@
 from flask import Blueprint, request, jsonify
 from config.function_jwt import create_token, validate_token
+from os import getenv
 
 routes_auth = Blueprint("routes_auth", __name__)
 
 @routes_auth.route("/login", methods=["POST"])
 def login():
-    data = request.get_json()
-    if data['username'] == "Admin":
-        return create_token(data)
-    else:
-        response = jsonify({"error": "User not found"})
-        response.status_code = 401
-        return response
+    try:
+        data = request.get_json()
+        if data['username'] == getenv("USERNAME"):
+            return create_token(data)
+        else:
+            response = jsonify({"error": "User not found"})
+            response.status_code = 401
+            return response
+    except:
+        return jsonify({"status": "Internal Error"}), 501
 
 @routes_auth.route("/verify/token")
 def verify():
-    token = request.headers['Authorization'].split(" ")[1]
-    return validate_token(token, output=True)
+    try:
+        token = request.headers['Authorization'].split(" ")[1]
+        return validate_token(token, output=True)
+    except:
+        return jsonify({"status": "Internal Error"}), 501
+
