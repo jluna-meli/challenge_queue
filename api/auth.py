e66f23bb3a9bbfa2619c34eff3fb36dc118ee1d1
jluna-meli <jessica.deluna@mercadolibre.com.mx>
2022-11-24 13:22:29 -0600

    [FIX_API] Update auth file and delete __init__ file from api package

--- api/auth.py
+++ api/auth.py
@@ -1,28 +1,14 @@
-from flask import Blueprint, request, jsonify
-from utils.function_jwt import create_token, validate_token
-from os import getenv
+from flask import Blueprint
+from services import auth
 
 routes_auth = Blueprint("routes_auth", __name__)
 
+
 @routes_auth.route("/login", methods=["POST"])
 def login():
-    try:
-        data = request.get_json()
-        user = getenv("USERNAME")
-        if data['username'] == user:
-            return create_token(data)
-        else:
-            response = jsonify({"error": "User not found"})
-            response.status_code = 401
-            return response
-    except Exception as e:
-        return jsonify({"status": "Internal Error"}), 501
+    return auth.login()
+
 
 @routes_auth.route("/verify/token")
 def verify():
-    try:
-        token = request.headers['Authorization'].split(" ")[1]
-        return validate_token(token, output=True)
-    except:
-        return jsonify({"status": "Internal Error"}), 501
-
+    return auth.verify()
