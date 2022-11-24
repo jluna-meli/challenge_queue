f3d2466b6b52323926ce1bc4ca089918e7794f67
jluna-meli <jessica.deluna@mercadolibre.com.mx>
2022-11-24 13:37:18 -0600

    [FIX_TEST] Unit_test_ Queue_formatting

--- tests/unit_tests/u_queue_test.py
+++ tests/unit_tests/u_queue_test.py
@@ -1,20 +1,11 @@
-import os
-
-import fakeredis
-
-import api.auth
-import services.auth
-
 try:
     import pytest
     import redis
     import tests.redis_tests.mocked_variables as mv
     import requests
+    import os
 except Exception as e:
     print("Some Modules are  Missing {} ".format(e))
-
-# r = redis.Redis(host="redis-container", port=6379, db=0)
-r = fakeredis.FakeStrictRedis(version=6)
 
 
 @pytest.mark.parametrize("json, status_code, message",
@@ -23,7 +14,7 @@ r = fakeredis.FakeStrictRedis(version=6)
                           (mv.msg_null, 400, b'{"error":"Message cannot be a null"}\n')])
 def test_0_r_push_successful(app, mocker, client, json, status_code, message, auth):
     """
-    Test when push a message successful
+    Test Push message in queue
     """
     token = auth
     mocker.patch("services.queue.push_redis", return_value=1)
@@ -53,7 +44,7 @@ def test_3__r_count(auth, mocker, client):
                                                                  b'because there not messages in queue"}\n')])
 def test_4_r_pop(auth, mocker, client, size, status_code, message):
     """
-    Test Pop one message
+    Test Pop messages
     """
     token = auth
     mocker.patch("services.queue.counter_messages", return_value=size)
@@ -67,6 +58,9 @@ def test_4_r_pop(auth, mocker, client, size, status_code, message):
 
 
 def test_6_validate_token(mocker, client, auth, verify_response_token):
+    """
+    Test validating token
+    """
     token = auth
 
     mocker.patch("services.auth.verify", return_value=verify_response_token)
@@ -78,30 +72,10 @@ def test_6_validate_token(mocker, client, auth, verify_response_token):
 
 def test_jwt_login(client, data_login):
     """
-    Creating a jwt token
+    Test loging
     """
     response = client.post("http://localhost:8000/api/login", json=data_login)
     assert response.status_code == 200
-
-
-def create_one_message(client, auth):
-    """
-    Creating at least one message
-    """
-    token = auth
-    response = client.post(mv.URL_QUEUE + "push", json=mv.msg_success,
-                           headers={"Authorization": f"Bearer {token.encode()}"})
-    print("this is ")
-    print(response)
-    return response
-
-
-def test_delete_all_messsges():
-    """
-    Delete all messages
-    """
-    while r.llen('Messages') != 0:
-        r.lpop('Messages')
 
 
 if __name__ == "__main__":
